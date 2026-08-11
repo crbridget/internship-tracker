601b4e792cf447e41e48d028ed56f7670c6fe84f
Bridget <crampton.b@northeastern.edu>
2026-08-11 19:16:13 -0400

    created 'companies' dictionary to contain a list of companies and their meta data that use greenhouse or lever. companies are added to the list when they pass the test for greenhouse or lever.

--- greenhouse_lever_check.py
+++ greenhouse_lever_check.py
@@ -1,25 +1,41 @@
 # Check if the company entered uses greenhouse or lever
 
 import requests
+from datetime import datetime
+
+companies = {}
+now = datetime.now()
 
 def normalize(company_name):
     return company_name.lower().replace(' ', '')
     
-def check_greenhouse(board_token):
+def check_greenhouse(company_name, board_token):
     url = f'https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs'
     response = requests.get(url)
     if response.status_code == 200:
         print(f"Success! {board_token} uses Greenhouse")
+        companies[company_name] = {
+            'token': board_token,
+            'source': 'greenhouse',
+            'verified_date': now
+        }
         return True
+        
+
     else:
         print(f"Failed. {board_token} does not use Greenhouse")
         return False
 
-def check_lever(board_token):
+def check_lever(company_name, board_token):
     url = f'https://api.lever.co/v0/postings/{board_token}?mode=json'
     response = requests.get(url)
     if response.status_code == 200:
         print(f"Success! {board_token} uses Lever")
+        companies[company_name] = {
+            'token': board_token,
+            'source': 'greenhouse',
+            'verified_date': now
+        }
         return True
     else:
         print(f"Failed. {board_token} does not use Lever")
@@ -28,5 +44,7 @@ def check_lever(board_token):
 if __name__ == "__main__":
     company = input("Enter your company: ")
     token = normalize(company)
-    check_greenhouse(token)
-    check_lever(token)
+    check_greenhouse(company, token)
+    check_lever(company, token)
+    print(companies)
+
