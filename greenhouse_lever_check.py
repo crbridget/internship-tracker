ed5dc9bb0cf5d8564cff99501f3c3ca5bc830240
Bridget <crampton.b@northeastern.edu>
2026-08-11 19:22:40 -0400

    add documentation

--- greenhouse_lever_check.py
+++ greenhouse_lever_check.py
@@ -7,9 +7,20 @@ companies = {}
 now = datetime.now()
 
 def normalize(company_name):
+    """ 
+    Normalize the company name for testing. 
+    input: company_name (str)
+    output: normalized comapany_name (str)
+    """
     return company_name.lower().replace(' ', '')
     
 def check_greenhouse(company_name, board_token):
+    """ 
+    Make an API call to greenhouse job board using 
+    the normalized comapny name as the board token.
+    input: company_name (str), board_token(str)
+    output: success or fail message (str), True or False (boolean)
+    """
     url = f'https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs'
     response = requests.get(url)
     if response.status_code == 200:
@@ -17,7 +28,8 @@ def check_greenhouse(company_name, board_token):
         companies[company_name] = {
             'token': board_token,
             'source': 'greenhouse',
-            'verified_date': now
+            'verified_date': now,
+            'status': 'active'
         }
         return True
         
@@ -27,6 +39,12 @@ def check_greenhouse(company_name, board_token):
         return False
 
 def check_lever(company_name, board_token):
+    """ 
+    Make an API call to lever job board using 
+    the normalized comapny name as the board token.
+    input: company_name (str), board_token(str)
+    output: success or fail message (str), True or False (boolean)
+    """
     url = f'https://api.lever.co/v0/postings/{board_token}?mode=json'
     response = requests.get(url)
     if response.status_code == 200:
@@ -34,12 +52,14 @@ def check_lever(company_name, board_token):
         companies[company_name] = {
             'token': board_token,
             'source': 'greenhouse',
-            'verified_date': now
+            'verified_date': now,
+            'status': 'active'
         }
         return True
     else:
         print(f"Failed. {board_token} does not use Lever")
         return False
+    
 
 if __name__ == "__main__":
     company = input("Enter your company: ")
